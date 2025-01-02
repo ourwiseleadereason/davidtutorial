a733d273fc70fb2dff818647eb7b522e52a4694b
ourdearleadereason <ourgreatleadereason@gmail.com>
2025-01-02 16:33:12 -0600

    basic register fixed

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const cors=require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const mongoose = require('mongoose'); // Import mongoose
 const bcrypt=require('bcryptjs');
 const User = require('./models/User.js');
 const jwt=require('jsonwebtoken');
 const app=express();
 
-
+const bcryptSalt=bcrypt.genSaltSync(10);
 
 app.use(express.json());
 
@@ -16,15 +16,20 @@ app.use(cors({
     origin: 'http://localhost:5173',
   }));
 
-  mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res)=>{
     res.json('test ok');
 });
 
-app.post('/register', (req, res)=>{
+app.post('/register', async (req, res)=>{
     const {name, email, password}=req.body;
-    res.json({name, email, password});
+    const userDoc=await User.create({
+        name, 
+        email, 
+        password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
 });
 
 
